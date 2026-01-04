4083d0720ed9952646755d36df355c5a18acf9ea
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2026-01-04 01:04:40 -0500

    Add Equal validator.

---
+++ CodingFlow.FluentValidation.UnitTests/EqualValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using CodingFlow.FluentValidation.Validators;
+using FluentAssertions;
+using static CodingFlow.FluentValidation.Validations;
+
+namespace CodingFlow.FluentValidation.UnitTests;
+
+public class EqualValidatorTests
+{
+    [TestCase(5)]
+    public void Equal_Valid(int input)
+    {
+        var result = RuleFor(input)
+            .Equal(5)
+            .Result();
+
+        result.Should().BeEquivalentTo(new ValidationResult
+        {
+            IsValid = true,
+            Errors = []
+        });
+    }
+
+    [TestCase(5f, 20f)]
+    [TestCase("some string", "another string")]
+    public void EqualInvalid<T>(T input, T otherInput)
+    {
+        var result = RuleFor(input)
+            .Equal(otherInput)
+            .Result();
+
+        result.Should().BeEquivalentTo(new ValidationResult
+        {
+            IsValid = false,
+            Errors = [new() { Message = $"Value '{input}' of type {typeof(T)} is not equal to {otherInput}." }]
+        });
+    }
+}
---
+++ CodingFlow.FluentValidation/Validators/EqualValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace CodingFlow.FluentValidation.Validators;
+
+public static class EqualValidator
+{
+    public static FluentValidation<T> Equal<T>(this FluentValidation<T> validation, T input)
+    {
+        Validate(validation, input);
+
+        return validation;
+    }
+
+    private static void Validate<T>(FluentValidation<T> validation, T input)
+    {
+        validation.Validate(
+            () => validation.Input.Equals(input),
+            new ValidationError($"Value '{validation.Input}' of type {typeof(T)} is not equal to {input}.")
+        );
+    }
+}
