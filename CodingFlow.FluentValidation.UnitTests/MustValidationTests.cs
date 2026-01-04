21f043c93b03955d2a4f9971afe17ef783cf574d
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2026-01-03 22:48:31 -0500

    Add custom predicate support.

---
+++ CodingFlow.FluentValidation.UnitTests/MustValidationTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using static CodingFlow.FluentValidation.Validations;
+
+namespace CodingFlow.FluentValidation.UnitTests;
+
+public class MustValidationTests
+{
+    [TestCase(5)]
+    public void Must_Valid(int input)
+    {
+        var result = RuleFor(input)
+            .Must(value => true)
+            .Result();
+
+        result.Should().BeEquivalentTo(new ValidationResult
+        {
+            IsValid = true,
+            Errors = []
+        });
+    }
+
+    [TestCase(5f)]
+    public void Must_Invalid(float input)
+    {
+        var result = RuleFor(input)
+            .Must(value => false)
+            .Result();
+
+        result.Should().BeEquivalentTo(new ValidationResult
+        {
+            IsValid = false,
+            Errors = [new() { Message = $"Value '{input}' of type System.Single is not valid." }]
+        });
+    }
+}
---
+++ CodingFlow.FluentValidation/MustValidation.cs
@@ -0,0 +1,27 @@
+﻿namespace CodingFlow.FluentValidation;
+
+public static class MustValidation
+{
+    extension<T>(FluentValidation<T> validation)
+    {
+        public FluentValidation<T> Must(Func<T, bool> predicate)
+        {
+            MustValidate<T>(validation, predicate);
+
+            return validation;
+        }
+    }
+
+    private static void MustValidate<T>(FluentValidation<T> validation, Func<T, bool> predicate)
+    {
+        var isValid = predicate(validation.Input);
+
+        if (!isValid)
+        {
+            validation.Result.IsValid = false;
+
+            validation.Errors.Add(
+                new ValidationError($"Value '{validation.Input}' of type {typeof(T)} is not valid."));
+        }
+    }
+}
