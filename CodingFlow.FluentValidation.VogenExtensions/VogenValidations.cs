180ce69cf5f595a45bec55f098de75d76228d50c
CodingFlow <3643313+CodingFlow@users.noreply.github.com>
2026-01-05 01:13:19 -0500

    - Include documentation comments in build so it gets included in nuget packages. - Add documentation comment to Vogen extension.

--- CodingFlow.FluentValidation.VogenExtensions/VogenValidations.cs
+++ CodingFlow.FluentValidation.VogenExtensions/VogenValidations.cs
@@ -4,6 +4,14 @@ namespace CodingFlow.FluentValidation.VogenExtensions;
 
 public static class VogenValidations
 {
+    /// <summary>
+    /// Creates the validation results for the validation chain and returns it
+    /// Vogen pass or fail types.
+    /// </summary>
+    /// <typeparam name="R">Input type.</typeparam>
+    /// <param name="validation"></param>
+    /// <returns>On validation success a <see cref="Validation.Ok"/> or if
+    /// validation fails a <see cref="Validation.Invalid(string)"/>.</returns>
     public static Validation VogenResult<R>(this FluentValidation<R> validation)
     {
         validation.Result = validation.Result();
